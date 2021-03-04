720b42e1ce85c3fb1c3dd7d66b1b84278e12929a
tpparker4 <79471570+tpparker4@users.noreply.github.com>
2021-03-03 21:35:11 -0600

    Third Program

---
+++ LAW5ThomasParker.appstudio/forms/calc/calc.js
@@ -0,0 +1,25 @@
+/*
+
+number1 = Number(prompt("Please enter a number:"))
+
+number2 = Number(prompt("Please enter a number:"))
+
+
+
+function Calculate(number1, number2) {
+
+return number1 + number2
+
+}
+
+  
+
+let summedNumbers = Calculate(number1,number2)
+
+
+
+console.log(`The sum of ${number1} and ${number2} is ${summedNumbers}`)
+
+alert(`The sum of the first and second number is ${summedNumbers}`)
+
+*/
