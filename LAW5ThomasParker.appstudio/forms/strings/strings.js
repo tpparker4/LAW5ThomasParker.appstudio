91ef6b57359160b8333f0c7299293413f5d64540
tpparker4 <79471570+tpparker4@users.noreply.github.com>
2021-03-03 21:39:45 -0600

    Fourth Branch

---
+++ LAW5ThomasParker.appstudio/forms/strings/strings.js
@@ -0,0 +1,24 @@
+let quoteString = ("Failure is simply the opportunity to begin again this time more intelligently.")
+
+quoteString.toUpperCase
+
+alert(quoteString)
+
+
+
+let authorString = (" - Henry Ford")
+
+alert(authorString)
+
+
+
+let completeString = quoteString.concat(authorString)
+
+alert(completeString)
+
+
+
+completeString.includes("Henry")
+
+alert(completeString.includes("Henry"))
+
